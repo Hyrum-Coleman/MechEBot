575f70fe569f3ea4254a3e2a10528bfcdbe3449b
nawper02 <nawper02@gmail.com>
2023-01-13 12:27:43 -0700

    Removed auto message

--- responses.py
+++ responses.py
@@ -12,5 +12,3 @@ def get_response(message: str) -> str:
 
     if p_message == '!help':
         return '`This is a help message that you can modify.`'
-
-    return 'I didn\'t understand what you wrote. Try typing "!help".'
