1f0cef5aa4a3e4c2a40ffc2e3ee32300e67aa7e9
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-14 01:40:38 -0700

    added events and made ping ephemeral

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const { token } = require('./config.json');
 // create a new Discord client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
@@ -20,33 +33,9 @@ for (const file of commandFiles) {
     if('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
     } else {
-        console.log('[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.');
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
 }
-
-// when the client is ready, run this code (only once)
-// using c as the event parameter
-client.once(Events.ClientReady, c => {
-    console.log('Ready! Logged in as ' + c.user.tag);
-})
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
 
 // login to Discord with our app's token
 client.login(token);
