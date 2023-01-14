694826e643587917c4a4c4b1d30703f398acd27d
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-14 15:18:20 -0700

    /role no longer allows all existing roles
    it also only applies to the command user

--- commands/role.js
+++ commands/role.js
@@ -8,48 +8,42 @@ module.exports = {
             subcommand
                 .setName('add')
                 .setDescription('adds a role to a user')
-                .addRoleOption((option) =>
+                .addStringOption((option) =>
                     option
                         .setName('role')
                         .setDescription('the role to add')
                         .setRequired(true)
+                        .addChoices(
+                            {'name': 'MechE', 'value': '1063531932568268803'},
+                        )
                 )
-
-                .addUserOption((option) =>
-                    option
-                        .setName('user')
-                        .setDescription('the user to add the role to')
-                        .setRequired(true)
-                ))
+        )
         .addSubcommand((subcommand) =>
             subcommand
                 .setName('remove')
                 .setDescription('removes a role from a user')
-                .addRoleOption((option) =>
+                .addStringOption((option) =>
                     option
                         .setName('role')
                         .setDescription('the role to remove')
                         .setRequired(true)
+                        .addChoices(
+                            {'name': 'MechE', 'value': '1063531932568268803'},
                 )
-                .addUserOption((option) =>
-                    option
-                        .setName('user')
-                        .setDescription('the user to remove the role from')
-                        .setRequired(true)
-                )
+        ),
         ),
 
     async execute(interaction) {
         try {
             const subcommand = interaction.options.getSubcommand();
             if (subcommand === 'add') {
-                const role = interaction.options.getRole('role');
-                const user = interaction.options.getUser('user');
+                const role = interaction.options.getString('role');
+                const user = interaction.member;
                 await interaction.guild.members.cache.get(user.id).roles.add(role);
                 await interaction.reply({content: `Added ${role} to ${user}`, ephemeral: false});
             } else if (subcommand === 'remove') {
-                const role = interaction.options.getRole('role');
-                const user = interaction.options.getUser('user');
+                const role = interaction.options.getString('role');
+                const user = interaction.member
                 await interaction.guild.members.cache.get(user.id).roles.remove(role);
                 await interaction.reply({content: `Removed ${role} from ${user}`, ephemeral: false});
             }
