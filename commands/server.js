c7b9ec62afa48f44f20f2e2a8c72e235e504ea62
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-14 01:06:43 -0700

    moved to discord.js

---
+++ commands/server.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server info!'),
+    async execute(interaction) {
+        await interaction.reply(`This server is ${interaction.guild.name}, and its ID is ${interaction.guild.id}`)
+    }
+}
