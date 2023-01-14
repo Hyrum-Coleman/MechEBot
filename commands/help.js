f3c58c73160337fbbdd9c71109d995d76ca92b7f
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-14 12:01:34 -0700

    /help is now using embeds

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
-// make command /help which will show all commands and their descriptions
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Get a list of all available commands!'),
-    async execute(interaction) {
-        // get all commands
-        const commands = interaction.client.commands;
-        // create a string to hold all commands
-        let commandList = '';
-        // loop through all commands
-        for (const command of commands.values()) {
-            // add the command's name and description to the string
-            commandList += `/${command.data.name} - ${command.data.description}\n`;
-        }
-        await interaction.reply({content: commandList, ephemeral: true})
+    async execute(interaction, client) {
+        const embed = new EmbedBuilder()
+            .setTitle('MechEBot Help Menu')
+            .setDescription('List of all available commands')
+            .setColor(0x18e1ee)
+            .setTimestamp(Date.now())
+            .addFields([
+                {
+                    name: '`/help`',
+                    value: 'Displays this help menu!',
+                },
+                {
+                    name: '`/ping`',
+                    value: 'Replies with Pong!',
+                }
+                
+            ]);
+
+        await interaction.reply({embeds: [embed]})
     },
 };
