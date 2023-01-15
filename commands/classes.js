fea7b2ac04fdcbe61002713822872ffc9eb3982a
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-14 18:12:45 -0700

    office hours now using embed

--- commands/classes.js
+++ commands/classes.js
@@ -1,58 +1,58 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const classes_collection = [
     { name: 'thermodynamics-001', value: 'meen2300-001'},
-    { name: 'ece-001', value: 'ece2210-001'},
+    { name: 'ece-004', value: 'ece2210-004'},
+    {name: 'manuf-for-eng-sys-001', value: 'meen2650-001'},
 ];
 
 function get_office_hours(course) {
     if (course === 'meen2300-001') {
-        let info = "```PROFESSOR HOURS\n"
-        info += 'Wednesday and Friday, 1:00 - 2:15.\n';
-        info += 'On Wednesday, the office hours are on zoom, and the password is 006740.\n';
-        info += 'On Friday, the office hours are in person in MEK 3352.\n\n';
-        info += "TA HOURS\n";
-        info += 'Thursdays from 12:00 - 1:00 on zoom.\n```';
-        return info;
+        let professor_hours = 'Wednesday and Friday, 1:00 - 2:15.\n\n';
+        professor_hours += 'On Wednesday, the office hours are on zoom, and the password is 006740.\n';
+        professor_hours += 'On Friday, the office hours are in person in MEK 3352.\n\n';
+
+        let ta_hours = 'Thursdays from 12:00 - 1:00 on zoom.';
+        return { professor_hours, ta_hours };
     }
-    if (course === 'ece2210-001') {
-        let info = "```PROFESSOR HOURS\n"
-        info += 'Mondays and Wednesdays after class until 12:15 pm in MEB 2278.\n';
-        info += 'Professor Simpson also provides office hours by appointment, either in person or through zoom.\n\n';
-        info += "TA HOURS\n";
-        info += 'Mondays:\n' +
-            '\t2:00 - 3:30 PM\n' +
-            '\t\tEmma\n' +
-            '\t\tRoom MEB 2267 and through Zoom:\n' +
-            '\t\thttps://utah.zoom.us/j/5099844531 Links to an external site. \n' +
-            '\t\tMeeting ID: 509 984 4531\n' +
-            '\n' +
+    else if (course === 'ece2210-004') {
+        let professor_hours = 'Mondays and Wednesdays after class until 12:15 pm in MEB 2278.\n\n';
+        professor_hours += 'Professor Simpson also provides office hours by appointment, either in person or through zoom.\n\n';
 
+        let ta_hours = 'Mondays:\n' +
+            'TA: Emma\n' +
+            '2:00 - 3:30 PM\n' +
+            'Room MEB 2267 and through Zoom:\n' +
+            'https://utah.zoom.us/j/5099844531\n' +
+            '\n' +
             'Tuesdays:\n' +
-            '\t4:00 - 5:30 PM\n' +
-            '\t\tRich\n' +
-            '\t\tRoom MEB 2267\n' +
+            'TA: Rich\n' +
+            '4:00 - 5:30 PM\n' +
+            'Room MEB 2267\n' +
             '\n' +
-
             'Wednesdays:\n' +
-            '\t2:00 - 3:30 PM\n' +
-            '\t\tMd Golam Dastgir\n' +
-            '\t\tRoom MEB 2255A\n' +
+            'TA: Md Golam Dastgir\n' +
+            '2:00 - 3:30 PM\n' +
+            'Room MEB 2255A\n' +
             '\n' +
-
             'Thursdays: \n' +
-            '\t2:00 - 3:30 PM\n' +
-            '\t\tBraden\n' +
-            '\t\tRoom MEB 2255A\n' +
+            'TA: Braden\n' +
+            '2:00 - 3:30 PM\n' +
+            'Room MEB 2255A\n' +
             '\n' +
-
             'Fridays:\n' +
-            '\t12:00 - 1:30 PM\n' +
-            '\t\tKyle\n' +
-            '\t\tRoom MEB 2255B\n' +
-            '\t\t3:00 - 4:30 PM\n' +
-            '\t\tUtpal\n' +
-            '\t\tRoom MEB 2255A in Week 1 and MEB 2255B thereafter\n```';
-        return info;
+            'TA: Kyle\n' +
+            '12:00 - 1:30 PM\n' +
+            'Room MEB 2255B\n\n' +
+            'TA: Utpal\n' +
+            '3:00 - 4:30 PM\n' +
+            'Room MEB 2255B\n';
+        return { professor_hours, ta_hours };
+    }
+    else if (course === 'meen2650-001') {
+        let professor_hours = 'Tuesdays and Thursdays from 10:30 - 11:30 in MEK 2339\n\n'
+        professor_hours += 'Professor Kuhl also provides office hours by appointment, either in person or through zoom.';
+        let ta_hours = 'none';
+        return { professor_hours, ta_hours };
     }
 }
 
@@ -113,7 +113,14 @@ module.exports = {
             if (subcommand === 'office-hours') {
                 const course = interaction.options.getString('class');
                 const office_hours = get_office_hours(course);
-                await interaction.reply({content: office_hours, ephemeral: false});
+                const embed = new EmbedBuilder()
+                    .setTitle(`Office Hours for ${course.toUpperCase()}`)
+                    .setColor(0x00FF00)
+                    .addFields([
+                        { name: '`Professor Hours`', value: office_hours.professor_hours },
+                        { name: '`TA Hours`', value: office_hours.ta_hours },
+                    ]);
+                await interaction.reply({embeds: [embed], ephemeral: false});
             } else if (subcommand === 'final-exam') {
                 const course = interaction.options.getString('class');
                 const final_exam = get_final_exam(course);
