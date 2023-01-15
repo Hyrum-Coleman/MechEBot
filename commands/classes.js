8e55e4b5d73bb1fa2b460079c8b5aab3d4f98ba9
nawper02 <nawper02@gmail.com>
2023-01-14 17:15:49 -0700

    Added office hours

--- commands/classes.js
+++ commands/classes.js
@@ -1,8 +1,71 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const classes_collection = [
     { name: 'thermodynamics-001', value: 'meen2300-001'},
-    { name: 'fluid-mechanics-001', value: 'meen2400-001'},
+    { name: 'ece-001', value: 'ece2210-001'},
 ];
+
+function get_office_hours(course) {
+    if (course === 'meen2300-001') {
+        let info = "```PROFESSOR HOURS\n"
+        info += 'Wednesday and Friday, 1:00 - 2:15.\n';
+        info += 'On Wednesday, the office hours are on zoom, and the password is 006740.\n';
+        info += 'On Friday, the office hours are in person in MEK 3352.\n\n';
+        info += "TA HOURS\n";
+        info += 'Thursdays from 12:00 - 1:00 on zoom.\n```';
+        return info;
+    }
+    if (course === 'ece2210-001') {
+        let info = "```PROFESSOR HOURS\n"
+        info += 'Mondays and Wednesdays after class until 12:15 pm in MEB 2278.\n';
+        info += 'Professor Simpson also provides office hours by appointment, either in person or through zoom.\n\n';
+        info += "TA HOURS\n";
+        info += 'Mondays:\n' +
+            '\t2:00 - 3:30 PM\n' +
+            '\t\tEmma\n' +
+            '\t\tRoom MEB 2267 and through Zoom:\n' +
+            '\t\thttps://utah.zoom.us/j/5099844531 Links to an external site. \n' +
+            '\t\tMeeting ID: 509 984 4531\n' +
+            '\n' +
+
+            'Tuesdays:\n' +
+            '\t4:00 - 5:30 PM\n' +
+            '\t\tRich\n' +
+            '\t\tRoom MEB 2267\n' +
+            '\n' +
+
+            'Wednesdays:\n' +
+            '\t2:00 - 3:30 PM\n' +
+            '\t\tMd Golam Dastgir\n' +
+            '\t\tRoom MEB 2255A\n' +
+            '\n' +
+
+            'Thursdays: \n' +
+            '\t2:00 - 3:30 PM\n' +
+            '\t\tBraden\n' +
+            '\t\tRoom MEB 2255A\n' +
+            '\n' +
+
+            'Fridays:\n' +
+            '\t12:00 - 1:30 PM\n' +
+            '\t\tKyle\n' +
+            '\t\tRoom MEB 2255B\n' +
+            '\t\t3:00 - 4:30 PM\n' +
+            '\t\tUtpal\n' +
+            '\t\tRoom MEB 2255A in Week 1 and MEB 2255B thereafter\n```';
+        return info;
+    }
+}
+
+function get_final_exam(course) {
+    if (course === 'meen2300-001') {
+        let info = "Placeholder\n";
+        return info;
+    }
+    if (course === 'ece2210-001') {
+        let info = "Placeholder\n";
+        return info;
+    }
+}
 
 
 module.exports = {
@@ -49,13 +112,11 @@ module.exports = {
             const subcommand = interaction.options.getSubcommand();
             if (subcommand === 'office-hours') {
                 const course = interaction.options.getString('class');
-                console.log(course);
-                const office_hours = 'information about office hours';
+                const office_hours = get_office_hours(course);
                 await interaction.reply({content: office_hours, ephemeral: false});
             } else if (subcommand === 'final-exam') {
                 const course = interaction.options.getString('class');
-                console.log(course);
-                const final_exam = 'information about final exam';
+                const final_exam = get_final_exam(course);
                 await interaction.reply({content: final_exam, ephemeral: false});
             }
         } catch (error) {
