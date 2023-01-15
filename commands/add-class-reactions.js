f079a5f9c70e1417c6c682fd7977d60d7b2050f0
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-15 15:00:15 -0700

    added /add-class-reactions command

---
+++ commands/add-class-reactions.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('add-class-reactions')
+        .setDescription('add reactions to a message for class roles')
+        .addStringOption((option) =>
+            option
+                .setName('message-id')
+                .setDescription('the message id to add reactions to')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
+        try {
+            const message = await interaction.channel.messages.fetch(interaction.options.getString('message-id'));
+            message.react('🔥')
+            message.react('⚡')
+            message.react('📚')
+            message.react('📈')
+            message.react('🏭')
+            message.react('🤓')
+            message.react('🌊')
+            message.react('🖥️')
+            message.react('🅰️')
+
+
+            await interaction.reply({ content: 'Reactions added!', ephemeral: true });
+        } catch (error) {
+            console.error(error);
+        }
+    },
+};
