9ce99c50c8c2de8bb3a8c099a423215e7fee95e4
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-26 23:35:02 -0700

    *more detail

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
             .addFields([
                 {
                     name: '`/help`',
-                    value: 'displays this help menu in a much longer message',
+                    value: 'displays this help menu in a much longer message featuring more length',
                 },
                 {
                     name: '`/role`',
