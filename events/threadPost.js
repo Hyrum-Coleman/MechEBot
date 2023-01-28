c2b0f6afd873c32d4f3a5b43658d48eaa449d336
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-01-28 16:12:59 -0700

    works but the tags are out of order

--- events/threadPost.js
+++ events/threadPost.js
@@ -10,77 +10,69 @@ module.exports = {
                     channel.send({embeds: [embed]});
                 });
             }
+            // function getThreadMessage() {
+            //     thread.messages.fetch(thread.messages.lastMessageId).then(message => {
+            //
+            //         let lastMessage = message
+            //     });
+            //     return lastMessage
+            // }
 
-            // wait 5 seconds to make sure the thread is created
-
+            // setTimeout(() => {
+            //     const threadMessage = getThreadMessage();
 
             const embed = new EmbedBuilder()
                 .setTitle(`New Homework Question!`)
-                // I want the embed description to be the first message in the thread, but I can't figure out how to get it
                 .setDescription(`${thread.name}`)
-                .setColor(0x424549)
+                .setColor(0x36393F)
                 .setTimestamp(Date.now())
                 .addFields([
                     {
-                        name: 'Question:',
-                        value: `${thread.lastMessage}`
+                        name: 'To answer this question, click the link below:',
+                        value: `[Click Here](${thread.url})`,
                     },
-                    {
-                        name: 'Link to Thread',
-                        value: thread.url,
-                    },
-                    ])
+                ])
 
             const thread_tag = thread.appliedTags[0];
             if (thread_tag === null) {
-            }
-            else if (thread_tag === '1062055127277654026') {
+            } else if (thread_tag === '1062055127277654026') {
                 let channel_id = '1054199680260657232'; // Thermodynamics
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062054945580384346') {
+            } else if (thread_tag === '1062054945580384346') {
                 let channel_id = '1054199694009573486'; // ECE
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062055029315473528') {
+            } else if (thread_tag === '1062055029315473528') {
                 let channel_id = '1062039365762691072'; // PDEs
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1067912348679012362') {
+            } else if (thread_tag === '1067912348679012362') {
                 let channel_id = '1062039712556130434'; // PDEs + Vector Calc
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062055654933676142') {
+            } else if (thread_tag === '1062056054827008112') {
                 let channel_id = '1054199720781807686'; // Statistics
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062055186144698491') {
+            } else if (thread_tag === '1062055186144698491') {
                 let channel_id = '1054199740226613258'; // Manufacturing
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062056054827008112') {
-                let channel_id = '1062046410587000942'; // Fluids
+            } else if (thread_tag === '') {
+                let channel_id = '1062055654933676142'; // Fluids
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062055360875208764') {
+            } else if (thread_tag === '1062055360875208764') {
                 let channel_id = '1063274753529495632'; // Numerical Methods
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1063281491330736158') {
+            } else if (thread_tag === '1063281491330736158') {
                 let channel_id = '1065308478236799128'; // ODEs + Lin Al
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1062055732251476090') {
+            } else if (thread_tag === '1062055732251476090') {
                 let channel_id = '1066526891567222844'; // Materials Science
                 await sendMsg(channel_id, embed);
-            }
-            else if (thread_tag === '1066530895680520252') {
+            } else if (thread_tag === '1066530895680520252') {
                 let channel_id = '1067910754960949319'; // Dynamics
                 await sendMsg(channel_id, embed);
-            }
-            else {
+            } else {
                 return;
             }
+            // },  1000)
+
         }
         catch (error) {
             console.error(error);
