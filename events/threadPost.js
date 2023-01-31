25d520fbb07f2596b49bc31c3ca7139936daf636
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2023-01-31 09:26:49 -0700

    fixed all the tags
    ready to be pushed to main

--- events/threadPost.js
+++ events/threadPost.js
@@ -16,6 +16,7 @@ module.exports = {
                 .setDescription(`${thread.name}`)
                 .setColor(0x36393F)
                 .setTimestamp(Date.now())
+                .setThumbnail('https://cdn.discordapp.com/attachments/1068279559239237652/1070017063856377956/5-53868_question-mark-clipart-question-mark-clip-art-question-red-question-mark-png.png')
                 .addFields([
                     {
                         name: 'To answer this question, click the link below:',
@@ -23,7 +24,8 @@ module.exports = {
                     },
                 ])
 
-            const thread_tag = thread.appliedTags[0];
+            const thread_tag = thread.appliedTags[0]; // Get the tag of the thread
+
             if (thread_tag === null) {
             } else if (thread_tag === '1062055127277654026') {
                 let channel_id = '1054199680260657232'; // Thermodynamics
@@ -34,28 +36,28 @@ module.exports = {
             } else if (thread_tag === '1062055029315473528') {
                 let channel_id = '1062039365762691072'; // PDEs
                 await sendMsg(channel_id, embed);
-            } else if (thread_tag === '1067912348679012362') {
+            } else if (thread_tag === '1062055360875208764') {
                 let channel_id = '1062039712556130434'; // PDEs + Vector Calc
                 await sendMsg(channel_id, embed);
             } else if (thread_tag === '1062056054827008112') {
-                let channel_id = '1054199720781807686'; // Statistics
+                let channel_id = '1070016071232393217'; // Statistics
                 await sendMsg(channel_id, embed);
             } else if (thread_tag === '1062055186144698491') {
                 let channel_id = '1054199740226613258'; // Manufacturing
                 await sendMsg(channel_id, embed);
-            } else if (thread_tag === '') {
-                let channel_id = '1062055654933676142'; // Fluids
-                await sendMsg(channel_id, embed);
-            } else if (thread_tag === '1062055360875208764') {
-                let channel_id = '1063274753529495632'; // Numerical Methods
+            } else if (thread_tag === '1062055654933676142') {
+                let channel_id = '1062046410587000942'; // Fluids
                 await sendMsg(channel_id, embed);
             } else if (thread_tag === '1063281491330736158') {
+                let channel_id = '1063274753529495632'; // Numerical Methods
+                await sendMsg(channel_id, embed);
+            } else if (thread_tag === '1067912348679012362') {
                 let channel_id = '1065308478236799128'; // ODEs + Lin Al
                 await sendMsg(channel_id, embed);
-            } else if (thread_tag === '1062055732251476090') {
+            } else if (thread_tag === '1066530895680520252') {
                 let channel_id = '1066526891567222844'; // Materials Science
                 await sendMsg(channel_id, embed);
-            } else if (thread_tag === '1066530895680520252') {
+            } else if (thread_tag === '1062055732251476090') {
                 let channel_id = '1067910754960949319'; // Dynamics
                 await sendMsg(channel_id, embed);
             } else {
